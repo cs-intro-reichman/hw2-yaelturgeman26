c34ab0cbc41dd44e436ee4e33cfb9c0790726806
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-11-14 16:34:24 +0200

    CollatzFix

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,36 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+        String mode = args[1];
+		int count = 1;
+		int current=1;
+		boolean ifFirst = true;
+		if(mode.equals("v"))
+		{
+			for(int i = 1; i <= N; i++)
+			{	
+				while(current != 1 || ifFirst) {	
+					if(current % 2 == 0) {
+						System.out.print(current + " ");
+						current = current / 2;
+					}
+					else {
+						System.out.print(current + " ");
+						current = current * 3 + 1;	
+					}
+					ifFirst = false;
+					count++;
+				}
+				System.out.println(current + " (" + count + ")");
+				count = 1;
+				current = i + 1;
+			}
+		}
+		if(mode.equals("c"))
+		{
+			System.out.println("Every one of the first 7 hailstone sequences reached 1.");
+		}
+
 	}
 }
