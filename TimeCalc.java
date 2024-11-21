920f6a9716ed3440c067a458f3a79e41fd1e9b74
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-11-21 11:28:40 +0200

    TimeCalc2

--- TimeCalc.java
+++ TimeCalc.java
@@ -2,9 +2,8 @@ public class TimeCalc {
     public static void main(String[] args) {
         String time = args[0]; 
         int minutesToAdd = Integer.parseInt(args[1]); 
-        String[] timeParts = time.split(":");
-        int hours = Integer.parseInt(timeParts[0]);
-        int minutes = Integer.parseInt(timeParts[1]);
+        int hours = Integer.parseInt(time.substring(0,2));
+        int minutes = Integer.parseInt(time.substring(03,5));
         int totalMinutes = (hours * 60) + minutes + minutesToAdd;
         int newHours = (totalMinutes / 60) % 24;
         int newMinutes = totalMinutes % 60;
